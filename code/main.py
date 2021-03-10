e52013e2ae74b8f232888b2817bb0df3805e5141
Jonny <123@mail.ru>
2021-03-10 07:41:27 +0500

    123

--- code/main.py
+++ code/main.py
@@ -1,18 +1,77 @@
-import test
-from ozon_methods import product
+import shutil
+from typing import List, Optional
 
-# https://api-seller.ozon.ru/docs/
+from fastapi.responses import FileResponse
+from fastapi import Depends, FastAPI, HTTPException, File, UploadFile
+from sqlalchemy.orm import Session
 
-client_id = "123090"
-api_key = "87e400a8-a791-4cc5-ad98-82c032c90d5b"
-headers_d = {
-        "accept": "application/json",
-        "Client-Id": "123090",
-        "Api-Key": "87e400a8-a791-4cc5-ad98-82c032c90d5b",
-        "Content-Type": "application/json"
-    }
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 
-if __name__ == "__main__":
-    res = product.product_info(headers_d, "uraala019", 53503011, 231872934)
-    # res = str(product.get_product_info_by_product_id(headers_d, 53503011)['sku'])
-    print(res)
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+        user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/")  # , response_model=List[schemas.Item]
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return {"items": items}
+
+
+# @app.get("/images/{path}")
+@app.get("/")
+async def read_item(path_img: str = "day_img.jpg"):
+    path = "foodServer/images/" + path_img
+    return FileResponse(path)
+
+
+@app.post("/image")
+async def image(image: UploadFile = File(...)):
+    with open("foodServer/images/day_img.gif", "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {"filename": image.filename}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
---
+++ code/schemas.py
@@ -0,0 +1,100 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
+
+
+class Orders(BaseModel):
+    posting_number: str
+    order_id: str
+    order_number: str
+    status: str
+    cancel_reason_id: int
+    created_at: datetime
+    in_process_at: datetime
+
+
+class OrderCreate(Orders):
+    pass
+
+
+class OrderProducts(BaseModel):
+    posting_number: str
+    sku: int
+    name: str
+    quantity: int
+    offer_id: str
+    price: float
+
+
+class OrderProductsCreate(OrderProducts):
+    pass
+
+
+class OrderAnalytics(BaseModel):
+    posting_number: str
+    region: str
+    city: str
+    delivery_type: str
+    is_premium: bool
+    payment_type_group_name: str
+    warehouse_id: int
+    warehouse_name: str
+
+
+class OrderAnalyticsCreate(OrderAnalytics):
+    pass
+
+
+class OrderFinData(BaseModel):
+    posting_number: str
+    commission_amount: int
+    commission_percent: int
+    payout: int
+    product_id: int
+    old_price: float
+    price: float
+    total_discount_value: float
+    total_discount_percent: float
+    actions: Optional[list] = None
+    picking: Optional[str] = None
+    quantity: int
+    client_price: float
+
+
+class OrderFinDataCreate(OrderFinData):
+    pass
