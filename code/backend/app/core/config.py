4be72595cc069b98455fc561f4ae3f1827c04261
Jonny <123@mail.ru>
2021-03-14 11:18:36 +0500

    Убрал данные для авторизации из конфига и добавил в personal_data.py

--- code/backend/app/core/config.py
+++ code/backend/app/core/config.py
@@ -1,9 +1,10 @@
+from personal_data import PersonalData
 
 class Settings:
     API_V1_STR: str = "/api/v1"
     HEADERS: dict = {
         "accept": "application/json",
-        "Client-Id": "123090",
-        "Api-Key": "87e400a8-a791-4cc5-ad98-82c032c90d5b",
+        "Client-Id": PersonalData.OZON_AUTH["Client-Id"],
+        "Api-Key": PersonalData.OZON_AUTH["Api-Key"],
         "Content-Type": "application/json"
     }
