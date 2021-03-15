f7174da9cbadfc34917cf097a8faf235c617109d
Jonny <123@mail.ru>
2021-03-15 18:22:55 +0500

    limit read_orders 100 to 50

--- code/backend/app/api/api_v1/endpoints/orders.py
+++ code/backend/app/api/api_v1/endpoints/orders.py
@@ -19,7 +19,7 @@ router = APIRouter()
 def read_orders(
         db: Session = Depends(deps.get_db),
         skip: int = 0,
-        limit: int = 100,
+        limit: int = 50,
         # current_user: models.User = Depends(deps.get_current_active_user), -- для авторизации
 ) -> Any:
     """
