83730985bc7f2c1851a3c68ae5a5e79b9631b1ed
Jonny <123@mail.ru>
2021-03-15 18:02:40 +0500

    234

--- code/backend/app/main.py
+++ code/backend/app/main.py
@@ -13,9 +13,8 @@ app = FastAPI()
 app.include_router(api_router, prefix=Settings.API_V1_STR)
 
 # uvicorn code.backend.app.main:app --host 192.168.0.100 --port 80 --reload
+# uvicorn code.backend.app.main:app --host 192.168.0.101 --port 80 --reload
 
 
-# Добавить обновление по таймеру каждые 5 минут - upsert за последние сутки
-# Обновление всех товаров 1 раз в неделю.
 # Добавить остальные таблицы с заказом
 
