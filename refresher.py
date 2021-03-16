4503acacf8936c05680e4370f1d0d4d8b3d9345e
Jonny <123@mail.ru>
2021-03-16 20:03:25 +0500

    findata complete

--- refresher.py
+++ refresher.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 import requests
@@ -10,10 +11,13 @@ def refresher_bot(url_links: List[str]):
     while True:
         count += 1
         res_upsert = requests.get(url_links[0]).json()
+        requests.get(url_links[2]).json()
         if res_upsert['created']:
-            PersonalData.play_audio()
-        if count == 6:
+            os.system('deadbeef money.py')
+            print("New order!")
+        if count == PersonalData.COUNT_REQ:
             count = 0
+            requests.post(url_links[3])
             requests.post(url_links[1])
             print("Refresh status of orders done. Go to sleep")
         print("Refresh orders done. Go to sleep")
@@ -22,5 +26,8 @@ def refresher_bot(url_links: List[str]):
 
 if __name__ == '__main__':
     url = [PersonalData.HOST + "/api/v1/orders/upsert/?days=1",
-           PersonalData.HOST + "/api/v1/orders/update/by_status/"]
+           PersonalData.HOST + "/api/v1/orders/update/by_status/",
+           PersonalData.HOST + "/api/v1/findata/upsert/?days=1",
+           PersonalData.HOST + "/api/v1/findata/update/by_status/"]
     refresher_bot(url)
+    # os.system('deadbeef money.py')
